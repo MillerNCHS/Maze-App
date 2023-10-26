8b06cb6b54b313f3a407d9b2c41d157a2a84af9d
Guzal-Rustamova <111534805+Guzal-Rustamova@users.noreply.github.com>
2023-10-26 13:02:56 -0500

    Square class
    Implemented the square class and ran tests (through the SquareTest.java file).

---
+++ src/SquareTest.java
@@ -0,0 +1,9 @@
+public class SquareTest {
+    public static void main(String[] args) {
+        Square obj = new Square(1, 1, 1);
+        System.out.println(obj);
+        System.out.println(obj.getRow());
+        System.out.println(obj.getCol());
+        System.out.println(obj.getType());
+    }
+}
