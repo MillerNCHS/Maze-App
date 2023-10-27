a2d2e64fe3e21f58be71bbc9d50c136db52377ba
Guzal-Rustamova <111534805+Guzal-Rustamova@users.noreply.github.com>
2023-10-27 12:54:00 -0500

    mazeSkeleton
    We finished creating the mazeSketeton branch. Added methods (but didn't write them).

---
+++ src/Maze.java
@@ -0,0 +1,40 @@
+public class Maze {
+    
+    public Maze()
+    {
+
+    }
+
+    /*
+    *@param String fname
+     */
+    public boolean loadMaze(String fname)
+    {
+
+    }
+
+    public ArrayList<Square> getNeighbors(Square sq)
+    {
+
+    }
+
+    public Square getStart()
+    {
+
+    }
+
+    public Square getFinish()
+    {
+
+    }
+
+    public void reset()
+    {
+
+    }
+
+    public String toString()
+    {
+        
+    }
+}
