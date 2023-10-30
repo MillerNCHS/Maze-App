054437ec05c3ca6be7f42e1c1802e7609799ca50
Bike Club Better <111458613+smkaszuba@users.noreply.github.com>
2023-10-30 12:59:48 -0500

    changes

--- src/Square.java
+++ src/Square.java
@@ -1,29 +1,48 @@
 public class Square {
-    int row, col, type;
-    
+    int row, col, type, initialType;
+
     public Square(int row, int col, int type) {
         this.row = row;
         this.col = col;
         this.type = type;
+        this.initialType = type;
     }
 
-    public int getRow() { return this.row; }
-    public int getCol() { return this.col; }
-    public int getType() { return this.type; }
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+
+    public int getType() {
+        return this.type;
+    }
 
     public String toString() {
-        switch(type) {
-            // come back to later 
-            case 0: 
+        switch (type) {
+            // come back to later
+            case 0:
                 return "_";
-            case 1: 
+            case 1:
                 return "#";
-            case 2: 
+            case 2:
                 return "S";
             case 3:
                 return "E";
+            case 4:
+                return "o";
+            case 5:
+                return ".";
+            case 6:
+                return "x";
             default:
-                return "aoweihfae";
+                return "error";
         }
+    }
+
+    public void reset() {
+        type = initialType;
     }
 }
