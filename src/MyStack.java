fd64d03e9122b270bbcd8c7f192920d6c1aac8dd
Bike Club Better <111458613+smkaszuba@users.noreply.github.com>
2023-11-01 13:23:50 -0500

    f

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,43 +1,36 @@
 import java.util.NoSuchElementException;
-import java.util.Stack; 
+import java.util.Stack;
 
 public class MyStack implements StackADT {
 
-    private Stack <Square> squares; 
+    private Stack<Square> squares;
 
-    public Square pop() throws NoSuchElementException
-    {
-        Square element = squares.remove(size()-1); 
-        return element; 
+    public Square pop() throws NoSuchElementException {
+        Square element = squares.remove(size() - 1);
+        return element;
     }
 
-    public void push(Square item)
-    {
-        squares.add(item); 
+    public void push(Square item) {
+        squares.add(item);
     }
 
-    public Square top() throws NoSuchElementException
-    {
-        Square element = squares.remove(size()-1); 
+    public Square top() throws NoSuchElementException {
+        Square element = squares.remove(size() - 1);
         squares.add(size(), element);
         return element;
-        
-    }
-
-    public int size()
-    {
-        
-    }
-
-    public boolean isEmpty()
-    {
 
     }
 
-    public void clear()
-    {
+    public int size() {
 
     }
-    
-    
+
+    public boolean isEmpty() {
+
+    }
+
+    public void clear() {
+
+    }
+
 }
