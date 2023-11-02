4180f22435e533c78f1f5538e0c13bec2f5bc209
Guzal-Rustamova <gurustamova@stu.naperville203.org>
2023-11-01 21:10:07 -0500

    updated myStack class and fixed some errors

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,30 +1,45 @@
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
-import java.util.Stack;
+
 
 public class MyStack<T> implements StackADT<T> {
 
-    private Stack<Square> squares;
-    private ArrayList <T> myStack = new ArrayList<>(); 
+    private ArrayList <T> myStack; 
+    int size = 0; 
+
+    public MyStack()
+    {
+        this.myStack = new ArrayList<>();
+    }
 
     public T pop() throws NoSuchElementException {
+        if (this.isEmpty())
+        {
+            throw new NoSuchElementException();
+        }
         T element = myStack.remove(size() - 1);
+        size--;
         return element;
     }
 
     public void push(T item) {
+        size++;
         myStack.add(item);
     }
 
     public T top() throws NoSuchElementException
     {
+        if (this.isEmpty())
+        {
+            throw new NoSuchElementException();
+        }
         return myStack.get(size()-1);
         
     }
 
     public int size()
     {
-        return myStack.size(); 
+        return size; 
     }
 
     public boolean isEmpty()
@@ -40,6 +55,18 @@ public class MyStack<T> implements StackADT<T> {
     {
         myStack = new ArrayList<>(); 
 
+    }
+
+    public String toString()
+    {
+        String display = "["; 
+        for (T x: this.myStack)
+        {
+            display+=x+" "; 
+        }
+        display += " "; 
+
+        return display; 
     }
 
 }
