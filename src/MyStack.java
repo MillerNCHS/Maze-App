e020501727dfe93e149f3d742c4664be6e4cbf4c
Bike Club Better <111458613+smkaszuba@users.noreply.github.com>
2023-11-02 13:09:38 -0500

    message

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,48 +1,39 @@
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
-<<<<<<< HEAD
 public class MyStack<T> implements StackADT<T> {
 
-    private ArrayList<T> myStack = new ArrayList<>();
+    private ArrayList<T> myStack;
+    int size = 0;
 
-    public T pop() throws NoSuchElementException {
-        T element = myStack.remove(size() - 1);
-=======
-
-public class MyStack<T> implements StackADT<T> {
-
-    private ArrayList <T> myStack; 
-    int size = 0; 
-
-    public MyStack()
-    {
+    public MyStack() {
         this.myStack = new ArrayList<>();
     }
 
     public T pop() throws NoSuchElementException {
-        if (this.isEmpty())
-        {
+        if (this.isEmpty()) {
             throw new NoSuchElementException();
         }
         T element = myStack.remove(size() - 1);
         size--;
->>>>>>> 280b1f100fde27bd9dd6e611e52dc283309deeef
         return element;
     }
 
     public void push(T item) {
-<<<<<<< HEAD
+        size++;
         myStack.add(item);
     }
 
     public T top() throws NoSuchElementException {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        }
         return myStack.get(size() - 1);
 
     }
 
     public int size() {
-        return myStack.size();
+        return size;
     }
 
     public boolean isEmpty() {
@@ -55,52 +46,16 @@ public class MyStack<T> implements StackADT<T> {
     public void clear() {
         myStack = new ArrayList<>();
 
-=======
-        size++;
-        myStack.add(item);
     }
 
-    public T top() throws NoSuchElementException
-    {
-        if (this.isEmpty())
-        {
-            throw new NoSuchElementException();
+    public String toString() {
+        String display = "[";
+        for (T x : this.myStack) {
+            display += x + " ";
         }
-        return myStack.get(size()-1);
-        
-    }
+        display += " ";
 
-    public int size()
-    {
-        return size; 
-    }
-
-    public boolean isEmpty()
-    {
-        if (myStack.get(0)==null)
-        {
-            return true; 
-        }
-        return false; 
-    }
-
-    public void clear()
-    {
-        myStack = new ArrayList<>(); 
-
-    }
-
-    public String toString()
-    {
-        String display = "["; 
-        for (T x: this.myStack)
-        {
-            display+=x+" "; 
-        }
-        display += " "; 
-
-        return display; 
->>>>>>> 280b1f100fde27bd9dd6e611e52dc283309deeef
+        return display;
     }
 
 }
--- src/mazeTest.java
+++ src/mazeTest.java
@@ -2,8 +2,6 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class mazeTest {
-
-<<<<<<< HEAD
     public static void main(String[] args) {
         // Square sq = new Square(1, 1, 1);
         Maze maze = new Maze();
@@ -19,24 +17,6 @@ public class mazeTest {
         maze.reset();
         System.out.println(maze.toString());
 
-=======
-    public static void main(String[] args)
-    {
-        //Square sq = new Square(1, 1, 1);
-        Maze maze = new Maze();
-        maze.loadMaze("src/maze-1"); 
-
-        System.out.println(maze.toString()); 
-
-        System.out.println(maze.getNeighbors(maze.getStart()));
-        
-        System.out.println(maze.getStart()); 
-        System.out.println(maze.getFinish()); 
-
-        maze.reset();
-        System.out.println(maze.toString()); 
-        
->>>>>>> 280b1f100fde27bd9dd6e611e52dc283309deeef
     }
 
 }
