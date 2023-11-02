0bd82ba8e43ee89375803acfbfaeb1c9eed57efc
Bike Club Better <111458613+smkaszuba@users.noreply.github.com>
2023-11-02 13:06:25 -0500

    stack test

---
+++ src/MyStack.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class MyStack<T> implements StackADT<T> {
+
+    private ArrayList<T> myStack = new ArrayList<>();
+
+    public T pop() throws NoSuchElementException {
+        T element = myStack.remove(size() - 1);
+        return element;
+    }
+
+    public void push(T item) {
+        myStack.add(item);
+    }
+
+    public T top() throws NoSuchElementException {
+        return myStack.get(size() - 1);
+
+    }
+
+    public int size() {
+        return myStack.size();
+    }
+
+    public boolean isEmpty() {
+        if (myStack.get(0) == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public void clear() {
+        myStack = new ArrayList<>();
+
+    }
+
+}
