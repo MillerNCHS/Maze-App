e8732639d49b57d005094626831be019f96503ef
lakshyaa-nathan <111480891+lakshyaa-nathan@users.noreply.github.com>
2023-11-03 13:16:21 -0500

    Working on the Stack Class

---
+++ src/MyStack.java
@@ -0,0 +1,9 @@
+public class MyStack implements StackADT{
+    public Stack stack = new Stack (Square)<>;
+
+     void push(T item){
+        stack.push(item);
+     }
+
+     System.out.println("hello");
+}
