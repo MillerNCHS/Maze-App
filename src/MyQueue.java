48de7b4aaf411838b59c915789f4d7e1014140ec
ksrinivasan11 <111408308+ksrinivasan11@users.noreply.github.com>
2023-11-06 12:32:58 -0600

    worked on queue

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,4 +1,5 @@
 class Node{
+    
     Square data;
     Node next;
      Node(Square content){
@@ -7,9 +8,64 @@ class Node{
     }
 }
 public class MyQueue implements QueueADT<Square>{
+    
     Node front, rear;
+
+
     public MyQueue(){
         this.front = null;
         this.rear = null;
     }
+
+    public void enqueue(Square element){
+        
+        Node temp = new Node
+
+        if (this.rear = null) {
+            this.front = this.rear = temp;
+        }
+        else{
+            this.rear.next = temp;
+            this.rear = temp;
+        }
+    }
+
+
+
+    public void dequeue(){
+        
+        if (this.front == null)
+            return;
+    
+    Node temp = this.front;
+    this.front = this.front.next;
+
+    if (this.front == null)
+        this.rear = null;
+    
+    }
+    
+
+    
+    public Square front(){
+
+        return this.front.data;
+    }
+
+   
+    int size(){
+
+
+    }
+    
+    boolean isEmpty(){
+        if (this.rear = null)
+            return true;
+    }
+
+    void clear(){
+        
+        this.front.data.reset();
+    }
 }
+
