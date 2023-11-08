7413c2b30371fefe6b8711d6b7cc2cd77912566d
William Wang <65054034+MemeMasterJeff@users.noreply.github.com>
2023-11-07 23:36:49 -0600

    MazeSolver complete save step()

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,50 +1,64 @@
+import java.util.ArrayList;
+
 public abstract class MazeSolver {
-    
+
     protected Maze maze;
+    //this has to be a string for some reason? i was originally trying to do an arraylist of arrays. - will
+    protected String solutionCoords;
 
-    abstract void makeEmpty(){
+    /**
+     * create an empty worklist
+     */
+    abstract void makeEmpty();
 
+    /**
+     * return true of the worklist is empty
+     * @return boolean
+     */
+    abstract boolean isEmpty();
 
-    }
+    /**
+     * add the given square to the worklist
+     * @param sq
+     */
+    abstract void add(Square sq);
 
-    abstract boolean isEmpty(){
-        
-    }
+    /**
+     * return the "next" item from the worklist
+     * @return
+     */
+    abstract Square next();
 
-    abstract void add(Square sq){
-        
-    }
-
-    abstract Square next(){
-    
-    }
-
-    MazeSolver(Maze mz){
+    MazeSolver(Maze mz) {
 
         maze = mz;
         makeEmpty();
         add(maze.getStart());
     }
 
-    public boolean isSolved(){
+    public boolean isSolved() {
 
-        boolean sol = false;
-
-        if ()
+        return isEmpty() || solutionCoords != null;
     }
 
-    public String getPath()[
+    public String getPath() {
 
-        for (int i = 0; i < size(); i++)
-            
-    ]
+        if(solutionCoords == null){
+            return "No solution";
+        }
+        return solutionCoords;
+    }
 
     public Square step(){
 
     }
 
     public void solve(){
+        while(!isSolved()){
+            step();
+        }
 
+        this.maze.reset();
     }
 
 }
